3a402717c149428e4b4930c5f4b30cb60b0cc341
Aza Maulana <azamaulanaaa@gmail.com>
2024-07-07 11:31:08 +0700

    feat: add handle patching
    - direct patch
    - pattern patch

---
+++ src/patch.rs
@@ -0,0 +1,96 @@
+use crate::handle::{Handle, HandleSnapshotFlag};
+use crate::memory::{Memory, PageProtectionFlags};
+use crate::pattern::Pattern;
+
+use std::io::{ErrorKind, Read, Write};
+
+pub struct PatchHandle<'a> {
+    handle: &'a Handle,
+}
+
+impl<'a> PatchHandle<'a> {
+    pub fn new(handle: &'a Handle) -> Self {
+        Self { handle }
+    }
+
+    fn read(&self, addr: usize, buf: &mut [u8]) -> Result<usize, ErrorKind> {
+        let mut memory = Memory::new(self.handle, addr, usize::MAX);
+        let n = memory.read(buf).map_err(|e| e.kind())?;
+        Ok(n)
+    }
+
+    pub fn direct<const M: usize>(&self, addr: usize, value: &[u8; M]) -> Result<&Self, ErrorKind> {
+        let mut memory = Memory::new(self.handle, addr, addr + M);
+        let _ = memory.write(value).map_err(|e| e.kind())?;
+        Ok(self)
+    }
+
+    pub fn pattern_matching<'b, const N: usize, const M: usize>(
+        &self,
+        module_name: Option<&'b str>,
+        pattern: Pattern<N>,
+        offset: usize,
+        value: &[u8; M],
+        step: usize,
+    ) -> Result<&Self, ErrorKind> {
+        match &module_name {
+            Some(module_name) => {
+                let module = self
+                    .handle
+                    .create_snapshot(
+                        HandleSnapshotFlag::SnapModule | HandleSnapshotFlag::SnapModule32,
+                    )?
+                    .get_modules()
+                    .find(|e| e.get_name() == *module_name)
+                    .ok_or(ErrorKind::NotFound)?;
+
+                let mut data = vec![0u8; module.get_size() as usize];
+                let n = self.read(module.get_address(), &mut data)?;
+                let data = &data[0..n];
+
+                let addr = data
+                    .windows(N)
+                    .step_by(step)
+                    .position(|e| e == pattern)
+                    .map(|addr| addr * step + offset)
+                    .ok_or(ErrorKind::NotFound)?;
+
+                self.direct(module.get_address() + addr, value)?;
+            }
+            None => {
+                for mbi in self.handle.get_memory_basic_informations() {
+                    // NOTE: This might not correct filter to memory page
+                    if mbi.get_protect().intersects(
+                        PageProtectionFlags::NoAccess
+                            | PageProtectionFlags::TargetsInvalid
+                            | PageProtectionFlags::Guard,
+                    ) || mbi.get_protect().is_empty()
+                    {
+                        continue;
+                    }
+
+                    let mut data = vec![0u8; mbi.get_region_size()];
+                    let n = self.read(mbi.get_base_address(), &mut data)?;
+                    let data = &data[0..n];
+
+                    let addr = data
+                        .windows(N)
+                        .step_by(step)
+                        .position(|e| e == pattern)
+                        .map(|addr| addr * step + offset)
+                        .ok_or(ErrorKind::NotFound);
+
+                    match addr {
+                        Ok(addr) => {
+                            self.direct(mbi.get_base_address() + addr, value)?;
+                            break;
+                        }
+                        Err(_) => continue,
+                    }
+                }
+            }
+        }
+
+        Ok(self)
+    }
+}

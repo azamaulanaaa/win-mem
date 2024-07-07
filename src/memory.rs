fd83e4af17693ef9ab9dbd5d5e788d22207ca6e4
Aza Maulana <azamaulanaaa@gmail.com>
2024-07-07 10:37:07 +0700

    feat: add memory basic information wrapper

--- src/memory.rs
+++ src/memory.rs
@@ -1,5 +1,9 @@
+use bitflags::bitflags;
 use std::io::{ErrorKind, Read, Seek, SeekFrom, Write};
 use windows::Win32::System::Diagnostics::Debug::{ReadProcessMemory, WriteProcessMemory};
+use windows::Win32::System::Memory::{
+    MEMORY_BASIC_INFORMATION, PAGE_PROTECTION_FLAGS, PAGE_TYPE, VIRTUAL_ALLOCATION_TYPE,
+};
 
 use crate::handle::Handle;
 
@@ -111,5 +115,128 @@ impl<'a> Seek for Memory<'a> {
                 Ok(self.current_address as u64)
             }
         };
+    }
+}
+
+bitflags! {
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+    pub struct PageProtectionFlags: u32 {
+        const Execute = 0x10;
+        const ExecuteRead = 0x20;
+        const ExecuteReadWrite = 0x40;
+        const ExecuteWriteCopy = 0x80;
+        const NoAccess = 0x01;
+        const ReadOnly = 0x02;
+        const ReadWrite = 0x04;
+        const WriteCopy = 0x08;
+        const TargetsInvalid = 0x40000000;
+        const TargetNoUpdate = 0x40000000;
+
+        const Guard = 0x100;
+        const NoCache = 0x200;
+        const WriteCombine = 0x400;
+    }
+}
+
+impl Into<PAGE_PROTECTION_FLAGS> for PageProtectionFlags {
+    fn into(self) -> PAGE_PROTECTION_FLAGS {
+        PAGE_PROTECTION_FLAGS(self.bits())
+    }
+}
+
+impl TryFrom<PAGE_PROTECTION_FLAGS> for PageProtectionFlags {
+    type Error = ErrorKind;
+
+    fn try_from(value: PAGE_PROTECTION_FLAGS) -> Result<Self, Self::Error> {
+        Self::from_bits(value.0).ok_or(ErrorKind::Unsupported)
+    }
+}
+
+bitflags! {
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+    pub struct VirtualAllocationType: u32 {
+        const Commit = 0x1000;
+        const Free = 0x10000;
+        const Reserve = 0x2000;
+    }
+}
+
+impl Into<VIRTUAL_ALLOCATION_TYPE> for VirtualAllocationType {
+    fn into(self) -> VIRTUAL_ALLOCATION_TYPE {
+        VIRTUAL_ALLOCATION_TYPE(self.bits())
+    }
+}
+
+impl TryFrom<VIRTUAL_ALLOCATION_TYPE> for VirtualAllocationType {
+    type Error = ErrorKind;
+
+    fn try_from(value: VIRTUAL_ALLOCATION_TYPE) -> Result<Self, Self::Error> {
+        Self::from_bits(value.0).ok_or(ErrorKind::Unsupported)
+    }
+}
+
+bitflags! {
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+    pub struct PageType: u32 {
+        const Image = 0x1000000;
+        const Mapped = 0x40000;
+        const Private = 0x20000;
+    }
+}
+
+impl Into<PAGE_TYPE> for PageType {
+    fn into(self) -> PAGE_TYPE {
+        PAGE_TYPE(self.bits())
+    }
+}
+
+impl TryFrom<PAGE_TYPE> for PageType {
+    type Error = ErrorKind;
+
+    fn try_from(value: PAGE_TYPE) -> Result<Self, Self::Error> {
+        Self::from_bits(value.0).ok_or(ErrorKind::Unsupported)
+    }
+}
+
+pub struct MemoryBasicInformation(MEMORY_BASIC_INFORMATION);
+
+impl MemoryBasicInformation {
+    pub fn get_base_address(&self) -> usize {
+        self.0.BaseAddress as usize
+    }
+
+    pub fn get_allocation_base(&self) -> usize {
+        self.0.AllocationBase as usize
+    }
+
+    pub fn get_allocation_protect(&self) -> PageProtectionFlags {
+        PageProtectionFlags::try_from(self.0.AllocationProtect).unwrap()
+    }
+
+    #[cfg(target_arch = "x86_64")]
+    pub fn get_partition_id(&self) -> u16 {
+        self.0.PartitionId
+    }
+
+    pub fn get_region_size(&self) -> usize {
+        self.0.RegionSize
+    }
+
+    pub fn get_state(&self) -> VirtualAllocationType {
+        VirtualAllocationType::try_from(self.0.State).unwrap()
+    }
+
+    pub fn get_protect(&self) -> PageProtectionFlags {
+        PageProtectionFlags::try_from(self.0.Protect).unwrap()
+    }
+
+    pub fn get_type(&self) -> PageType {
+        PageType::try_from(self.0.Type).unwrap()
+    }
+}
+
+impl From<MEMORY_BASIC_INFORMATION> for MemoryBasicInformation {
+    fn from(value: MEMORY_BASIC_INFORMATION) -> Self {
+        Self(value)
     }
 }

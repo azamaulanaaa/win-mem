94137a942db800c356f5c01f712c506ef028cf52
Aza Maulana <azamaulanaaa@gmail.com>
2024-07-09 11:32:54 +0700

    feat!: rewrite patch to utilizing rust enum

--- src/patch.rs
+++ src/patch.rs
@@ -4,134 +4,133 @@ use crate::pattern::Pattern;
 
 use std::io::{ErrorKind, Read, Write};
 
+/// memory section available for pattern matching
+pub enum MemorySection<'a> {
+    /// all memory section that patchable
+    All,
+    /// memory section that hold module binary like exe or dll
+    Module(&'a str),
+}
+
+/// differnt way to get the base address for patching
+pub enum BaseAddress<'a, const N: usize> {
+    /// patch directly to the given address
+    Direct(usize),
+    /// verify value of the address before patching
+    DirectVerify(usize, Pattern<N>),
+    /// find value in the memory section that matches the pattern to get the address
+    Pattern(Pattern<N>, MemorySection<'a>),
+}
+
 /// Patching a process
 pub struct PatchHandle<'a> {
     handle: &'a Handle,
 }
 
 impl<'a> PatchHandle<'a> {
+    /// create new instance for patching memory of the handle
     pub fn new(handle: &'a Handle) -> Self {
         Self { handle }
+    }
+
+    /// applying patches based on given config
+    pub fn apply<'b, const N: usize, const M: usize, const K: usize>(
+        &self,
+        base_address: BaseAddress<N>,
+        offsets: Option<&[usize; K]>,
+        value: &[u8; M],
+    ) -> Result<&Self, ErrorKind> {
+        let mut addr: usize = match base_address {
+            BaseAddress::Direct(addr) => Ok(addr),
+            BaseAddress::DirectVerify(addr, pattern) => {
+                let mut data = vec![0u8; N];
+                let _ = self.read(addr, &mut data);
+                if pattern == data.as_slice() {
+                    Ok(addr)
+                } else {
+                    Err(ErrorKind::NotFound)
+                }
+            }
+            BaseAddress::Pattern(pattern, mem_section) => {
+                let step = 4;
+
+                let address_ranges: Vec<(usize, usize)> = match mem_section {
+                    MemorySection::All => {
+                        let mut address_ranges = Vec::new();
+                        for mbi in self.handle.get_memory_basic_informations() {
+                            // NOTE: This might not correct filter to memory page
+                            if mbi.get_protect().intersects(
+                                PageProtectionFlags::NoAccess
+                                    | PageProtectionFlags::TargetsInvalid
+                                    | PageProtectionFlags::Guard,
+                            ) || mbi.get_protect().is_empty()
+                            {
+                                continue;
+                            }
+
+                            address_ranges.push((mbi.get_base_address(), mbi.get_region_size()))
+                        }
+                        address_ranges
+                    }
+                    MemorySection::Module(module_name) => {
+                        let module = self
+                            .handle
+                            .create_snapshot(
+                                HandleSnapshotFlag::SnapModule | HandleSnapshotFlag::SnapModule32,
+                            )?
+                            .get_modules()
+                            .find(|e| e.get_name() == *module_name)
+                            .ok_or(ErrorKind::NotFound)?;
+
+                        vec![(module.get_address(), module.get_size() as usize)]
+                    }
+                };
+
+                let mut addr: Option<usize> = None;
+                for address_range in address_ranges {
+                    let mut data = vec![0u8; address_range.1 as usize];
+                    let n = self.read(address_range.0, &mut data)?;
+                    let data = &data[0..n];
+
+                    match data
+                        .windows(N)
+                        .step_by(step)
+                        .position(|e| e == pattern)
+                        .map(|addr| address_range.0 + addr * step)
+                    {
+                        Some(v) => {
+                            addr = Some(v);
+                            break;
+                        }
+                        None => continue,
+                    }
+                }
+
+                addr.ok_or(ErrorKind::NotFound)
+            }
+        }?;
+
+        match offsets {
+            Some(offsets) => {
+                let mut data = Vec::from(0usize.to_ne_bytes());
+
+                for offset in offsets {
+                    let _ = self.read(addr, &mut data);
+                    addr = usize::from_ne_bytes(*data.as_slice().first_chunk().unwrap()) + offset;
+                }
+            }
+            None => (),
+        }
+
+        let mut memory = Memory::new(self.handle, addr, addr + M);
+        let _ = memory.write(value).map_err(|e| e.kind())?;
+
+        Ok(self)
     }
 
     fn read(&self, addr: usize, buf: &mut [u8]) -> Result<usize, ErrorKind> {
         let mut memory = Memory::new(self.handle, addr, usize::MAX);
         let n = memory.read(buf).map_err(|e| e.kind())?;
         Ok(n)
-    }
-
-    /// direct patching to an address without checking
-    pub fn direct<const M: usize>(&self, addr: usize, value: &[u8; M]) -> Result<&Self, ErrorKind> {
-        let mut memory = Memory::new(self.handle, addr, addr + M);
-        let _ = memory.write(value).map_err(|e| e.kind())?;
-        Ok(self)
-    }
-
-    /// direct patching but verity current value before patching
-    pub fn direct_verify<const N: usize, const M: usize>(
-        &self,
-        addr: usize,
-        pattern: Pattern<N>,
-        value: &[u8; M],
-    ) -> Result<&Self, ErrorKind> {
-        let mut data = vec![0u8; N];
-        let _ = self.read(addr, &mut data);
-        if pattern == data.as_slice() {
-            self.direct(addr, value)?;
-        } else {
-            return Err(ErrorKind::NotFound);
-        }
-
-        Ok(self)
-    }
-
-    /// direct patching with pointer
-    pub fn direct_pointer<const N: usize, const K: usize>(
-        &self,
-        addr: usize,
-        offsets: &[usize; K],
-        value: &[u8; N],
-    ) -> Result<&Self, ErrorKind> {
-        let mut addr = addr;
-        let mut data = Vec::from(0usize.to_ne_bytes());
-
-        for offset in offsets {
-            let _ = self.read(addr, &mut data);
-            addr = usize::from_ne_bytes(*data.as_slice().first_chunk().unwrap()) + offset;
-        }
-
-        self.direct(addr, value)?;
-
-        Ok(self)
-    }
-
-    /// patching to a first match address that have value matchs the pattern
-    pub fn pattern_matching<'b, const N: usize, const M: usize>(
-        &self,
-        module_name: Option<&'b str>,
-        pattern: Pattern<N>,
-        offset: usize,
-        value: &[u8; M],
-        step: usize,
-    ) -> Result<&Self, ErrorKind> {
-        match &module_name {
-            Some(module_name) => {
-                let module = self
-                    .handle
-                    .create_snapshot(
-                        HandleSnapshotFlag::SnapModule | HandleSnapshotFlag::SnapModule32,
-                    )?
-                    .get_modules()
-                    .find(|e| e.get_name() == *module_name)
-                    .ok_or(ErrorKind::NotFound)?;
-
-                let mut data = vec![0u8; module.get_size() as usize];
-                let n = self.read(module.get_address(), &mut data)?;
-                let data = &data[0..n];
-
-                let addr = data
-                    .windows(N)
-                    .step_by(step)
-                    .position(|e| e == pattern)
-                    .map(|addr| addr * step + offset)
-                    .ok_or(ErrorKind::NotFound)?;
-
-                self.direct(module.get_address() + addr, value)?;
-            }
-            None => {
-                for mbi in self.handle.get_memory_basic_informations() {
-                    // NOTE: This might not correct filter to memory page
-                    if mbi.get_protect().intersects(
-                        PageProtectionFlags::NoAccess
-                            | PageProtectionFlags::TargetsInvalid
-                            | PageProtectionFlags::Guard,
-                    ) || mbi.get_protect().is_empty()
-                    {
-                        continue;
-                    }
-
-                    let mut data = vec![0u8; mbi.get_region_size()];
-                    let n = self.read(mbi.get_base_address(), &mut data)?;
-                    let data = &data[0..n];
-
-                    let addr = data
-                        .windows(N)
-                        .step_by(step)
-                        .position(|e| e == pattern)
-                        .map(|addr| addr * step + offset)
-                        .ok_or(ErrorKind::NotFound);
-
-                    match addr {
-                        Ok(addr) => {
-                            self.direct(mbi.get_base_address() + addr, value)?;
-                            break;
-                        }
-                        Err(_) => continue,
-                    }
-                }
-            }
-        }
-
-        Ok(self)
     }
 }
